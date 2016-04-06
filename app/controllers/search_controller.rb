b0d632429aff1b13b428bd50ffaff7cb3d2dbf10
William Zerega <Bill@Williams-MacBook-Pro.local>
2016-04-06 14:25:39 -0400

    set up ajax calls

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,13 +5,11 @@ class SearchController < ApplicationController
 	def index
 		
 
-	respond_to do |format|
-      format.json do
         name = params[:name]
         email = params[:email]
         @user = User.create(:name => name, :email => email)
+        
         render json: @result
-      end
     end
 		
 		# render json: {itunes: @response}
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,12 @@ Bundler.require(*Rails.groups)
 module Ajaxpractice
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib) 
+    use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end  
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
