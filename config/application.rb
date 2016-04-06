159e756a48241233273c0c5b95efca8f662519b5
William Zerega <Bill@Williams-MacBook-Pro.local>
2016-04-06 14:30:42 -0400

    trying to get set up for ajax calls

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,16 @@ Bundler.require(*Rails.groups)
 module Ajaxpractice
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib) 
-    use Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: :any
-      end  
-    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
